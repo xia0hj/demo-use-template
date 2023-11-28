dcba10212078417624336281d0d6eb209de28b40
xia0hj <719887163@qq.com>
2023-11-29 00:40:24 +0800

    😋 fix path alias

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,11 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
+
+vi.mock('vscode', () => {
+  return {
+    window: vi.fn(),
+    workspace: vi.fn()
+  }
+})
 
 describe('should', () => {
   it('exported', () => {
